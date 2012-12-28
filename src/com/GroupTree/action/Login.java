7ff090b99f0fceacba7afbccda108458c88a8c84
povergoing <povergoing@gmail.com>
2012-12-28 22:08:29 +0800

    action Signed-off-by: povergoing <povergoing@gmail.com>

---
+++ src/com/GroupTree/action/Login.java
@@ -0,0 +1,55 @@
+package com.GroupTree.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Login extends ActionSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String username;
+	private String password;
+	
+	
+	
+	
+	
+	public String getUsername() {
+		return username;
+	}
+
+
+
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+
+
+	public String execute(){
+		
+		return "success";
+	}
+}
