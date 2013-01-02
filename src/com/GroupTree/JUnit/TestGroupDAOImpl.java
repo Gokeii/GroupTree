40e40c91d871fc54c18dc73b66c22a7564d4cf18
Gokeii <21250546@qq.com>
2013-01-03 04:01:21 +0800

    DAO done.
    Signed-off-by: Gokeii <21250546@qq.com>

--- src/com/GroupTree/JUnit/TestGroupDAOImpl.java
+++ src/com/GroupTree/JUnit/TestGroupDAOImpl.java
@@ -3,15 +3,18 @@ package com.GroupTree.JUnit;
 import org.junit.Test;
 
 import com.GroupTree.dao.impl.GroupDAOImpl;
+import com.GroupTree.dao.impl.UserDAOImpl;
 
 public class TestGroupDAOImpl {
 
 	@Test
 	public void testGetAllGroups() {
 		GroupDAOImpl groupDAO = new GroupDAOImpl();
+		UserDAOImpl userDAO = new UserDAOImpl();
 //		groupDAO.getAllGroups("s992025", "302010");
 		String username = "s992025";
 		String password = "302010";
-		groupDAO.getByID("ADM992", username, password);
+		groupDAO.getByID("K", username, password);
+		userDAO.getByID("u2501", username, password);
 	}
 }
