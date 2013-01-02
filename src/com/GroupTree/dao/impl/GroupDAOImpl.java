c93041a22fa85484044f54352227b651d4c6c1d2
Gokeii <21250546@qq.com>
2013-01-02 22:55:38 +0800

    get all groups method in GroupDAO done.
    Signed-off-by: Gokeii <21250546@qq.com>

--- src/com/GroupTree/dao/impl/GroupDAOImpl.java
+++ src/com/GroupTree/dao/impl/GroupDAOImpl.java
@@ -1,18 +1,78 @@
 package com.GroupTree.dao.impl;
 
+import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
-
 import com.GroupTree.dao.GroupDAO;
 import com.GroupTree.model.Group;
+import com.GroupTree.utils.MainframeCommandEntry;
 
 public class GroupDAOImpl implements GroupDAO{
 	
 	public Group getByID(String ID, String username, String password) {
-		return null;
+		Group group = new Group();
+		try {
+			String info = MainframeCommandEntry.enterCommand(username, password, "LU "+ID);
+			System.out.println(info);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return group;
 	}
 
 	public List<Group> getAllGroups(String username, String password) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Group> groups = new LinkedList<Group>();
+		try {
+			//get all available groups
+			String command = "SEARCH CLASS(GROUP)";
+			String info = MainframeCommandEntry.enterCommand(username, password, command);
+			
+			//get 'superior group' attribute of all available groups
+			List<String> groupsName = new LinkedList<String>();
+			String[] infoLines = info.split("\n");
+			for (int i = 2; i < infoLines.length; i++)
+				if (!infoLines[i].contains("READY"))
+					groupsName.add(infoLines[i].substring(1));
+				else break;
+			
+			String commands = "";
+			for (int i = 0; i < groupsName.size(); i++)
+				commands += "LG " + groupsName.get(i) + "\n  ";
+			
+			String infos = MainframeCommandEntry.enterCommand(username, password, commands);
+			String[] infosLines = infos.split("\n");
+			int i = 1;
+			Group group;
+			while (i < infosLines.length) {
+				if (infosLines[i].contains("READY")) {
+					i++;
+					continue;
+				}
+				if (infosLines[i].contains("END"))
+					break;
+				i++;
+				group = new Group();
+				
+				String[] tmp = infosLines[i].split(" ");
+				group.setId(tmp[tmp.length-1]);
+				i++;
+				tmp = infosLines[i].split(" ");
+				for (int j = 0; j < tmp.length; j++)
+					if (tmp[j].contains("GROUP")) {
+						group.setSuperiorGroup(tmp[j].split("=")[1]);
+						break;
+					}
+				groups.add(group);
+				
+				while (i < infosLines.length && !infosLines[i].contains("READY")) i++;
+				i++;
+			}
+			
+/*			for (int j = 0; j < groups.size(); j++)
+				System.out.println(j+": "+groups.get(j).getId()+"parent: "+groups.get(j).getSuperiorGroup());*/
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return groups;
 	}
 }
--- src/com/GroupTree/utils/MainframeLogin.java
+++ src/com/GroupTree/utils/MainframeLogin.java
@@ -1,33 +1,31 @@
 package com.GroupTree.utils;
 
+import java.io.IOException;
+
 import org.apache.commons.net.ftp.FTPClient;
 
 public class MainframeLogin {
 	public static boolean login(String userName, String password) {
 		String serverName = "10.60.37.129";
 		boolean status = false;
-		FTPClient ftp = new FTPClient();		
-		// Connect to the server
+		FTPClient ftp = new FTPClient();
+
 		try {
+			// Connect to the server
 			ftp.connect(serverName);
 			System.out.println(ftp.getReplyString());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		// Login to the server
-		try {
+		
+			// Login to the server
 			if ((status = ftp.login(userName, password)) == false)
 				System.out.println("Unable to log in to zOS as " + userName);
 			System.out.println(ftp.getReplyString());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		// Quit the server
-		try {
+		
+			// Quit the server
 			ftp.quit();
-		} catch (Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
 		return status;
 	}
 }
